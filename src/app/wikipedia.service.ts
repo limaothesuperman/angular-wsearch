304ac993052523e0669dcb7332e2574f103c532a
Mao Li <mli94@stevens.edu>
2023-06-12 16:09:10 -0500

    added interface of wikipediaResponse

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      pageid: number;
+      title: string;
+      snippet: number;
+    }[];
+  };
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +21,7 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
         format: 'json',
@@ -18,6 +30,8 @@ export class WikipediaService {
         srsearch: term,
         origin: '*'
       }
-    });
+    }).pipe(
+      pluck('query', 'search')
+    );
   }
 }
